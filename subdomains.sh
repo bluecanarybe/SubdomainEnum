7f736574aebc1eeaaeb97947ce1c4f73271ad86a
Blue Canary <59455457+bluecanarybe@users.noreply.github.com>
2021-03-30 16:00:23 +0200

    Update subdomains.sh
    Added suppport for double domains (e.g. test.domain.com)

--- subdomains.sh
+++ subdomains.sh
@@ -2,14 +2,26 @@
 # make sure a domain is given as an argument
 if [ -z "$*" ]; then echo "ERROR: no domain given as argument!"; fi
 
+# check number of domains in given argument
+countdots=$(echo $1 | grep -o "\." | wc -l)
+
 # prep directory
 rm -rf /tmp/$1 2>/dev/null
 rm -rf /root/tools/OneForAll/results/* 2>/dev/null
 mkdir /tmp/$1
 
 # variables
-domain=$(echo $1 | cut -d'.' -f1)
+if [ "$countdots" == "1" ]; then
+domain1=$(echo $1 | cut -d'.' -f1)
 extension=$(echo $1 | cut -d'.' -f2)
+elif [ "$countdots" == "2" ]; then
+domain1=$(echo $1 | cut -d'.' -f1)
+domain2=$(echo $1 | cut -d'.' -f2)
+extension=$(echo $1 | cut -d'.' -f3)
+else
+echo ''
+fi
+
 export CHAOS_KEY="56d69d525a018cb7ab1f7f275b99948aead70ad1a7481929e27a92b5782dc1d3"
 
 echo 'running Amass'
@@ -28,7 +40,7 @@ echo 'running Chaos'
 /root/go/bin/chaos -d $1 -silent -o /tmp/$1/chaos.tmp
 
 echo 'saving results in one file ...'
-cat /root/tools/OneForAll/results/temp*.txt /tmp/$1/amass.tmp /tmp/$1/chaos.tmp /tmp/$1/turbolist3r.tmp /tmp/$1/assetfinder.tmp > /tmp/$1/results1.tmp
+cat /root/tools/OneForAll/results/temp/*.txt /tmp/$1/amass.tmp /tmp/$1/chaos.tmp /tmp/$1/turbolist3r.tmp /tmp/$1/assetfinder.tmp > /tmp/$1/results1.tmp
 
 echo 'cleaning duplicates and showing results ...'
 
@@ -41,7 +53,14 @@ sed 's/\r//' < /tmp/$1/results2.tmp > /tmp/$1/results3.tmp
 #remove duplicates
 awk '!a[$0]++' /tmp/$1/results3.tmp > /tmp/$1/results4.tmp
 # remove false positives (non matching domain)
-egrep "\.$domain\.$extension$" /tmp/$1/results4.tmp > /tmp/$1/subdomains.txt
+
+if [ "$countdots" == "1" ]; then
+egrep ''$domain1'\.'$extension'' /tmp/$1/results4.tmp > /tmp/$1/subdomains.txt
+elif [ "$countdots" == "2" ]; then
+egrep ''$domain1'\.'$domain2'\.'$extension'' /tmp/$1/results4.tmp > /tmp/$1/subdomains.txt
+else
+echo ''
+fi
 
 RED='\033[0;31m'
 printf ''${RED}'---------------------- ENUMERATED SUBDOMAINS ----------------------\n'
@@ -53,5 +72,5 @@ cat /tmp/$1/subdomains.txt | /root/go/bin/httprobe -p http:8000 -p http:8080 -p 
 printf ''${RED}'--------------------- RUNNING RESPONSECHECKER ---------------------\n'
 /root/scripts/ResponseChecker/ResponseChecker /tmp/$1/http-subdomains.txt | tee /tmp/$1/responsecodes.tmp
 cat /tmp/$1/responsecodes.tmp | grep 200 | awk '{ print $1 }' > /tmp/$1/200-OK-urls.txt
-rm /tmp/$1/*.tmp
+#rm /tmp/$1/*.tmp
 printf ''${RED}'---------------------------- FINISHED -----------------------------\n'
